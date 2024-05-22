074516c629d86f5c9d565cd3ccff85d7b48e0fd1
Sabith <businessmansabith@gmail.com>
2024-05-22 11:06:02 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnlineNav: true,
+  swcMinify: true,
+  disableSwc: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
